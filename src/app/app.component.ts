a2c94830e774257661e40620d642083dae19b407
hao.xing <hao.xing@accenture.com>
2020-12-02 14:46:39 +0800

    xh - pwa_demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { RestApiService } from './rest-api.service';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { WebsocketService } from './websocket.service';
+import { ChatService } from './chat.service';
+export interface TableElement {
+  id: string;
+  name: string;
+  email: string;
+  website: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [WebsocketService, ChatService]
 })
+
 export class AppComponent {
-  title = 'pwa-demo';
+  Data: TableElement[];
+  col: string[] = ['id', 'name', 'email', 'website'];
+  dataSource;
+  subscription;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor(private restApiService: RestApiService, private chatService: ChatService) {
+
+    // chatService.messages.subscribe(msg => {
+    //   console.log('Response from websocket: ' + msg);
+    // });
+    this.subscription = this.chatService.getData()
+    .subscribe(val => {
+        console.log(val);
+    });
+
+    this.restApiService.getUsers().subscribe((res) => {
+      this.dataSource = new MatTableDataSource<TableElement>(res);
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+      }, 0);
+    });
+  }
+  private message = {
+    author: 'tutorialedge',
+    message: 'this is a test message'
+  };
+  sendMsg() {
+    console.log('new message from client to websocket: ', this.message);
+    // this.chatService.messages.next(this.message);
+    // this.message.message = '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    RouterModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
